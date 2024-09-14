93947b3a989f0d2147166c445f9683530cf7f6d6
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-09-14 17:50:14 +0100

    not equal operator

---
+++ main.py
@@ -0,0 +1,14 @@
+# part 1
+a = 10
+b = 10
+if a!=b:
+    print("Not equals")
+else:
+    print("equals")
+#part 2
+x = "Chiamalite"
+y = "chiamalite"
+if x!=y:
+    print("not equals")
+else:
+    print("similar name")
